77bf9b9f8fce6c7e5038cc51cb1b88100364ac39
Renato <renato.vini@outlook.com>
2022-10-29 11:56:13 -0300

    database

--- config/database.ts
+++ config/database.ts
@@ -49,6 +49,21 @@ const databaseConfig: DatabaseConfig = {
       healthCheck: false,
       debug: false,
     },
+    pg: {
+      client: 'pg',
+      connection: {
+        host: Env.get('DB_HOST', '127.0.0.1') as string,
+        port: Number(Env.get('DB_PORT', 5432)),
+        user: Env.get('DB_USER', 'postgres') as string,
+        password: Env.get('DB_PASSWORD', '') as string,
+        database: Env.get('DB_NAME', 'movies') as string,
+      },
+      healthCheck: false,
+      migrations: {
+        naturalSort: true,
+      },
+      debug: false,
+    },
     sqlite: {
       client: 'sqlite',
       connection: {
