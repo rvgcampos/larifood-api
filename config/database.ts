ca01accf4560cce8ef9359cf0e835078c1f507b6
Renato <renato.vini@outlook.com>
2022-10-31 01:10:21 -0300

    change var

--- config/database.ts
+++ config/database.ts
@@ -52,16 +52,16 @@ const databaseConfig: DatabaseConfig = {
     pg: {
       client: 'pg',
       connection: {
-        host: Env.get('DB_HOST', '127.0.0.1') as string,
-        port: Number(Env.get('DB_PORT', 5432)),
-        user: Env.get('DB_USER', 'postgres') as string,
-        password: Env.get('DB_PASSWORD', '') as string,
-        database: Env.get('DB_NAME', 'movies') as string,
+        host: Env.get('PG_HOST'),
+        port: Env.get('PG_PORT'),
+        user: Env.get('PG_USER'),
+        password: Env.get('PG_PASSWORD', ''),
+        database: Env.get('PG_DB_NAME'),
       },
-      healthCheck: false,
       migrations: {
         naturalSort: true,
       },
+      healthCheck: false,
       debug: false,
     },
     sqlite: {
